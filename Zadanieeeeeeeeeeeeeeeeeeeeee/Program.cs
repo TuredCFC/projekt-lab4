7290dd4ae621facf685c64b8fd24a5227528d99a
bbbartek674@gmail.com <bbbartek674@gmail.com>
2023-02-04 21:21:28 +0100

    wykonanie zadania

---
+++ Zadanieeeeeeeeeeeeeeeeeeeeee/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program oblicza pierwiastki równania kwadratowego ax^2 + bx + c = 0");
+            Console.Write("Podaj a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Równanie ma dwa różne pierwiastki: x1 = {0}, x2 = {1}", x1, x2);
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Równanie ma jeden pierwiastek x = {0}", x);
+            }
+            else
+            {
+                Console.WriteLine("Równanie nie ma pierwiastków rzeczywistych");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testyzadanieeeee/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xunit;
+namespace UnitTests
+{
+    public class QuadraticEquationTests
+    {
+        [Theory]
+        [InlineData(1, -3, 2, 2, 1)]
+        [InlineData(1, 0, -1, 1, -1)]
+        [InlineData(2, -4, 2, 1, 2)]
+        public void TestRealRoots(double a, double b, double c, double expectedRoot1, double expectedRoot2)
+        {
+            double root1, root2;
+            int numberOfRoots = FindRoots(a, b, c, out root1, out root2); Assert.Equal(2, numberOfRoots);
+            Assert.Equal(expectedRoot1, root1, (int)1e-6);
+            Assert.Equal(expectedRoot2, root2, (int)1e-6);
+        }
+        [Theory]
+        [InlineData(1, 2, 1)]
+        [InlineData(2, 4, 2)]
+        [InlineData(2, 2, 1)]
+        public void TestOneRealRoot(double a, double b, double c)
+        {
+            double root1, root2;
+            int numberOfRoots = FindRoots(a, b, c, out root1, out root2); Assert.Equal(1, numberOfRoots);
+            Assert.Equal(-b / (2 * a), root1, (int)1e-6);
+        }
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 3, 4)]
+        [InlineData(3, 4, 5)]
+        public void TestNoRealRoots(double a, double b, double c)
+        {
+            double root1, root2;
+            int numberOfRoots = FindRoots(a, b, c, out root1, out root2); Assert.Equal(0, numberOfRoots);
+        }
+        private int FindRoots(double a, double b, double c, out double root1, out double root2)
+        {
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                root1 = root2 = 0;
+                return 0;
+            }
+            if (discriminant == 0)
+            {
+                root1 = root2 = -b / (2 * a);
+                return 1;
+            }
+            double sqrtDiscriminant = Math.Sqrt(discriminant);
+            root1 = (-b + sqrtDiscriminant) / (2 * a);
+            root2 = (-b - sqrtDiscriminant) / (2 * a);
+            return 2;
+        }
+    }
+}
